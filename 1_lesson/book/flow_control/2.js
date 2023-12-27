d3caa765f065a709f2de59de0d3bef362bd15334
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-12-27 23:16:44 +0000

    Add if statement checking for integer

--- 1_lesson/book/flow_control/2.js
+++ 1_lesson/book/flow_control/2.js
@@ -1,16 +1,20 @@
 function evenOrOdd(number) {
-  let remainder = number % 2;
-  switch (remainder) {
-    case 0:
-      console.log('Even');
-      break;
-    case 1:
-      console.log('Odd');
-      break;
-    default:
-      console.log(remainder)
-      console.log('Something Went Wrong');
-      break;
+  if (Number.isInteger(number)) {
+    let remainder = number % 2;
+    switch (remainder) {
+      case 0:
+        console.log('Even');
+        break;
+      case 1:
+        console.log('Odd');
+        break;
+      default:
+        console.log(remainder)
+        console.log('Something Went Wrong');
+        break;
+    }
+  } else {
+  console.log('Given value not an integer');
   }
 }
 
