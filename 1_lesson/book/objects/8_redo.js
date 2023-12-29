cdc3307b49174c7da88d5605f7d44ebbe1eed1ed
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-12-29 08:32:38 +0000

    Add solution to objects exercise 8

---
+++ 1_lesson/book/objects/8_redo.js
@@ -0,0 +1,26 @@
+function copyObj (original, keys) {
+  let copy = {};
+
+  if (keys) {
+    keys.forEach(key => copy[key] = original[key]);
+  } else {
+    copy = Object.assign(copy, original);
+  }
+
+  return copy;
+}
+
+let objToCopy = {
+  foo: 1,
+  bar: 2,
+  qux: 3,
+};
+
+let newObj = copyObj(objToCopy);
+console.log(newObj);        // => { foo: 1, bar: 2, qux: 3 }
+
+let newObj2 = copyObj(objToCopy, [ 'foo', 'qux' ]);
+console.log(newObj2);       // => { foo: 1, qux: 3 }
+
+let newObj3 = copyObj(objToCopy, [ 'bar' ]);
+console.log(newObj3);       // => { bar: 2 }
