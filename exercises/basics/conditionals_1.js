31e478d93d8e4bb9e9a1f9919263630469698b13
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-12-30 17:28:14 +0000

    Add explanation for conditionals 1 exercise

---
+++ exercises/basics/conditionals_1.js
@@ -0,0 +1,32 @@
+const myBoolean = true;
+const myString = 'hello';
+const myArray = [];
+const myOtherString = '';
+
+if (myBoolean) {
+  console.log('Hello');
+}
+
+if (!myString) {
+  console.log('World');
+}
+
+if (!!myArray) {
+  console.log('World');
+}
+
+if (myOtherString || myArray) {
+  console.log('!');
+}
+
+/*
+THese are falsy values:
+  null
+  undefined
+  NaN
+  0
+  ''
+  false
+
+Everything else is truthy, including empty objects {} and empty arrays []
+*/
