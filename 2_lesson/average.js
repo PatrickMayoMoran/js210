f7c7f333912548cac5e89da23642e695b8c82650
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-12-30 19:06:06 +0000

    Add temperature example to average exercise

--- 2_lesson/average.js
+++ 2_lesson/average.js
@@ -6,14 +6,11 @@ function sum(array) {
   return total;
 }
 
-function average (num1, num2, num3) {
-  let average = sum(num1, num2, num3) / 3;
-  return average;
-}
-
 function arrayAverage (array) {
   let total = sum(array);
   return total / array.length;
 }
 
 console.log(arrayAverage([1, 3, 5, 7, 9, 11, 13, 15, 17]));
+let temperatures = [71, 44, 53, 59, 48];
+console.log(arrayAverage(temperatures));
