09ba775c8760fe2bd531c0e26e6752fb3fe3de83
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-12-30 00:29:58 +0000

    Practice nesting if statements

--- 1_lesson/apples_bananas.js
+++ 1_lesson/apples_bananas.js
@@ -3,8 +3,10 @@ let bananas = "3"
 
 if (apples === bananas) {
   console.log("Egad, they're equal!");
-} else if (apples == bananas) {
-  console.log("Same, different types");
 } else {
-  console.log("Not the same");
+    if (apples == bananas) {
+    console.log("Same, different types");
+  } else {
+    console.log("Not the same");
+  }
 }
