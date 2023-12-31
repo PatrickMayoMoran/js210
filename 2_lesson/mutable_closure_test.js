6795280091d23833dfa37f9dca3e9926d06f2c3e
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-12-31 19:57:30 +0000

    Add explanation for separate mutable object example

--- 2_lesson/mutable_closure_test.js
+++ 2_lesson/mutable_closure_test.js
@@ -11,9 +11,11 @@ function closureArray() {
 let array1 = closureArray();
 let array2 = closureArray();
 
+// each invocation of closureArray returns a function with a pointer to a new
+// Array object created on line 2 when closureArray is run.
 array1(); // [0, 1, 2, "More shoes!"]
 array1(); // [0, 1, 2, "More shoes!", "More shoes!"]
 array1(); // [0, 1, 2, "More shoes!", "More shoes!", "More shoes!"]
-array2(); // [0, 1, 2, "More shoes!", "More shoes!", "More shoes!", "More shoes!"]
 array2(); // [0, 1, 2, "More shoes!"]
-array2(); // [0, 1, 2, "More shoes!"]
+array2(); // [0, 1, 2, "More shoes!", "More shoes!"]
+array2(); // [0, 1, 2, "More shoes!", "More shoes!", "More shoes!"]
