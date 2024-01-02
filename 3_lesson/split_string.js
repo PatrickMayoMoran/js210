efef49c5ad27beec4d6ea986dad7b5a2d101e92e
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-01-02 02:45:05 +0000

    Add initial case for split string function

---
+++ 3_lesson/split_string.js
@@ -0,0 +1,34 @@
+function splitString(string, delimiter) {
+  if (delimiter === undefined) {
+    console.log('ERROR: No delimiter.');
+    return;
+  }
+  // …
+}
+
+splitString('abc,123,hello world', ',');
+// logs:
+// abc
+// 123
+// hello world
+
+splitString('hello');
+// logs:
+// ERROR: No delimiter
+
+splitString('hello', '');
+// logs:
+// h
+// e
+// l
+// l
+// o
+
+splitString('hello', ';');
+// logs:
+// hello
+
+splitString(';hello;', ';');
+// logs:
+//  (this is a blank line)
+// hello
