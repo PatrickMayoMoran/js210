041c987afaeffb563933642422a6bf6b52c8718c
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-01-02 10:45:00 +0000

    Add split string solution

--- 3_lesson/split_string.js
+++ 3_lesson/split_string.js
@@ -1,9 +1,32 @@
+/*
+ * If there's no delimiter, return undefined
+ * create a new empty string
+ * for the length of string, iterate over each character
+ * if the delimiter is blank, log each character
+ * otherwise, if character !== delimiter, add it to string
+ * otherwise, if character === delimiter, 
+ *  log the word
+ *  reset the word
+*/
+
 function splitString(string, delimiter) {
   if (delimiter === undefined) {
     console.log('ERROR: No delimiter.');
     return;
   }
-  // …
+
+  split = ''
+  for (let i = 0; i < string.length; i += 1) {
+    if (delimiter === '') {
+      console.log(string[i]);
+    } else if (string[i] !== delimiter) {
+      split += string[i];
+    } else if (string[i] === delimiter) {
+      console.log(split);
+      split = '';
+    }
+  }
+  if (!!split) console.log(split);
 }
 
 splitString('abc,123,hello world', ',');
