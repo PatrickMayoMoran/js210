bc913effc23f6f75fdcbfbde549a0d2af0edfa7e
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-01-02 23:20:18 +0000

    Add solution to multiples of three and five problem

---
+++ exercises/easy_1/multiples_three_five.js
@@ -0,0 +1,13 @@
+function multisum(number) {
+  let multiples = [];
+  for (let i = 0; i <= number; i += 1) {
+    if (i % 3 === 0 || i % 5 === 0) multiples.push(i);
+  }
+
+  return multiples.reduce((acc, val) => acc + val);
+}
+
+console.log(multisum(3));       // 3
+console.log(multisum(5));       // 8
+console.log(multisum(10));      // 33
+console.log(multisum(1000));    // 234168
