d4d25f4f9ebfde007d4a1cc13289b082408bf7ef
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-01-04 20:33:18 +0000

    Add solution to second object properties problem

--- 5_lesson/object_properties_1.js
+++ 5_lesson/object_properties_1.js
@@ -16,3 +16,21 @@ function objectHasProperty(object, key) {
 console.log(objectHasProperty(pets, 'dog'));       // true
 console.log(objectHasProperty(pets, 'lizard'));    // false
 console.log(objectHasProperty(pets, 'mice'));      // true
+
+let wins = {
+  steve: 3,
+  susie: 4,
+};
+
+function incrementProperty(object, key) {
+  if (objectHasProperty(object, key)) {
+    return object[key] += 1;
+  } else {
+    return object[key] = 1;
+  }
+}
+
+console.log(incrementProperty(wins, 'susie'));   // 5
+console.log(wins);                               // { steve: 3, susie: 5 }
+console.log(incrementProperty(wins, 'lucy'));    // 1
+console.log(wins);                               // { steve: 3, susie: 5, lucy: 1 }
