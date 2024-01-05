f5629e184020c3f9f03e251bab4b29f2901a2bcf
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-01-05 15:44:39 +0000

    Add prompt for repeated characters question

---
+++ 5_lesson/repeatedCharacters.js
@@ -0,0 +1,10 @@
+function repeatedCharacters(string) {
+  let lowerString = string.toLowerCase();
+  return lowerString;
+}
+
+console.log(repeatedCharacters('Programming'));    // { r: 2, g: 2, m: 2 }
+console.log(repeatedCharacters('Combination'));    // { o: 2, i: 2, n: 2 }
+console.log(repeatedCharacters('Pet'));            // {}
+console.log(repeatedCharacters('Paper'));          // { p: 2 }
+console.log(repeatedCharacters('Baseless'));       // { s: 3, e: 2 }
