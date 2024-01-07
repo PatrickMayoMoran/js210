c5b43e56819d04a685b2f0096826c77f1d42bc51
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-01-07 16:59:27 +0000

    Add solution to sugar 8

---
+++ 5_lesson/sugar_8.js
@@ -0,0 +1,10 @@
+const obj = {
+  first: "I am the first",
+  second: "I am the second",
+  third: [1, 2, 3],
+  rest: { a: 'a', b: 'b' },
+};
+
+let first = obj.first;
+let second = obj.secondl
+let rest = { third: obj.third, rest: obj.rest };
