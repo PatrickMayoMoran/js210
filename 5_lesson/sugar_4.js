e38a8a06b27f89667f9e75a71d485bec298b0106
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-01-07 16:38:13 +0000

    Add solution to sugar 4 exercise

---
+++ 5_lesson/sugar_4.js
@@ -0,0 +1,27 @@
+// classic version
+function foo(array) {
+  return [
+    array[2],
+    5,
+    array[0],
+  ];
+}
+
+let array = [1, 2, 3];
+let result = foo(array);
+let bar = result[0];
+let qux = result[1];
+let baz = result[2];
+
+// sugar version
+function foo([ one, , three ]) {
+  return [
+    three,
+    5,
+    one,
+  ];
+}
+
+let array = [1, 2, 3];
+let result = foo(array);
+let [ bar, qux, baz ] = result;
