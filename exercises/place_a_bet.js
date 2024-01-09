8adbaebf11854fee8fd909466a7c8fceb3358332
Patrick Moran <patrick.mayo.moran@gmail.com>
2024-01-09 17:45:10 +0000

    Add explanation for failed place a bet code

---
+++ exercises/place_a_bet.js
@@ -0,0 +1,21 @@
+function placeABet(guess) {
+  // function expression does not create a local variable for this function
+  (function generateRandomInt() {
+    return Math.floor(Math.random() * 25) + 1;
+  });
+
+  const winningNumber = generateRandomInt();
+
+  if (guess < 1 || guess > 25) {
+    return 'Invalid guess. Valid guesses are between 1 and 25.';
+  }
+
+  if (guess === winningNumber) {
+    return "Congratulations, you win!";
+  } else {
+    return "Wrong-o! You lose.";
+  }
+}
+
+const userGuess = 7;
+console.log(placeABet(userGuess));
